c0e3a6bc9cc1e8f623f5e50a61fac1883e522154
JoshGoold <joshgoold2@hotmail.com>
2024-09-04 11:59:48 -0400

    ui fixes 2.0

--- main/src/components/StockPage.jsx
+++ main/src/components/StockPage.jsx
@@ -141,11 +141,11 @@ const StockPage = () => {
     <div className='p-4'>
         <div className="text-2xl m p-10 flex justify-between shadow-md">
             <div className="flex t items-center"><img className='h-16 w-16' alt='Logo' src={icon || "N/A"}></img> &nbsp; <h1>{symbol} | {data.displayName}</h1></div>
-            <div className="flex p-3 n gap-2">
-            <button className='text-sm p-3 rounded-sm hover:scale-105 shadow-md' onClick={()=> nav("/")}>Home</button>
-            <button onClick={()=>setPageNav({stockAnalysis: true, financials: false, news: false})} className={`${pageNav.stockAnalysis === true ? "bg-blue-500 text-white" : ""} rounded-sm text-sm p-3 hover:scale-105 shadow-md`} >Stock Analysis</button>
-            <button onClick={()=>setPageNav({stockAnalysis: false, financials: true, news: false})} className={`${pageNav.financials === true ? "bg-blue-500 text-white" : ""} rounded-sm text-sm p-3 hover:scale-105 shadow-md`} >Financials</button>
-            <button onClick={()=>setPageNav({stockAnalysis: false, financials: false, news: true})} className={`${pageNav.news === true ? "bg-blue-500 text-white" : ""} rounded-sm text-sm p-3 hover:scale-105 shadow-md`} >News</button>
+            <div className="flex pt-3 n gap-2">
+            <button className='text-sm p rounded-sm hover:scale-105 shadow-md' onClick={()=> nav("/")}>Home</button>
+            <button onClick={()=>setPageNav({stockAnalysis: true, financials: false, news: false})} className={`${pageNav.stockAnalysis === true ? "bg-blue-500 text-white" : ""} rounded-sm text-sm p  hover:scale-105 shadow-md`} >Stock Analysis</button>
+            <button onClick={()=>setPageNav({stockAnalysis: false, financials: true, news: false})} className={`${pageNav.financials === true ? "bg-blue-500 text-white" : ""} rounded-sm text-sm p  hover:scale-105 shadow-md`} >Financials</button>
+            <button onClick={()=>setPageNav({stockAnalysis: false, financials: false, news: true})} className={`${pageNav.news === true ? "bg-blue-500 text-white" : ""} rounded-sm text-sm p  hover:scale-105 shadow-md`} >News</button>
             </div>
         </div> 
         {/* Company Metrics */}
