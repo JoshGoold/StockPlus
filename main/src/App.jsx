fa59f407f1ed35a72a1f41dd0a9702e9d51f7fcd
JoshGoold <joshgoold2@hotmail.com>
2024-09-03 20:11:55 -0400

    first commit

---
+++ main/src/App.jsx
@@ -0,0 +1,17 @@
+import Home from './dist/components/Home'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import StockPage from './dist/components/StockPage'
+
+function App() {
+
+  return (
+    <BrowserRouter>
+    <Routes>
+    <Route index element={<Home/>}/>
+    <Route path='/stocks/:symbol' element={<StockPage/>}/>
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
